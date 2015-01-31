bade15fcf1ea8a9fe193bd3c40129ef6b5cace8e
Ashish Kumar <ashish@ashish.co>
2015-01-31 19:20:05 +0100

    Cleanup

--- iCloud/ViewController.m
+++ iCloud/ViewController.m
@@ -8,37 +8,20 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UIDocumentPickerDelegate, UIDocumentInteractionControllerDelegate>
+@interface ViewController () <UIDocumentPickerDelegate>
 
-@property (nonatomic, strong) UIDocumentInteractionController *documentInteractionController;
-@property (nonatomic, strong) NSURL *documentURL;
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
+
 @implementation ViewController
-
-- (UIDocumentInteractionController *)documentInteractionController {
-    if (!_documentInteractionController) {
-        _documentInteractionController = [[UIDocumentInteractionController alloc] init];
-        _documentInteractionController.delegate = self;
-    }
-    
-    return _documentInteractionController;
-}
-
 
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     [self presentUIDocumentPicker];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
@@ -56,14 +39,6 @@
 
 - (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentAtURL:(NSURL *)url {
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
-    
-}
-
-
-#pragma mark - UIDocumentInteractionControllerDelegate
-
-- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller {
-    return self;
 }
 
 
